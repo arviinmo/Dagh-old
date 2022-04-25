7bf2d72cac3554d402929b576464a951f79892da
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-04-25 17:32:00 +0430

    remove tailwindcss from project

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React from "react";
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="flex items-center animate-pulse">
     </div>
   );
 }
