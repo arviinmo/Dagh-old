396b771eabcb7f9c36930794733cffe50dc30658
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-04-25 17:04:04 +0430

    customize cra default template

--- src/App.tsx
+++ src/App.tsx
@@ -19,27 +19,6 @@ function App() {
           ></path>
         </svg>
       </div>
-      <p className="mt-6 tracking-wide">
-        Edit <code>src/App.jsx</code> and save to reload.
-      </p>
-      <div className="flex justify-center mt-4">
-        <a
-          className="px-4 py-2 text-white bg-indigo-500 rounded hover:bg-indigo-600"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="px-4 py-2 ml-4 text-white bg-indigo-500 rounded hover:bg-indigo-600"
-          href="https://tailwindcss.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Tailwind CSS v3.x
-        </a>
-      </div>
     </div>
   );
 }
