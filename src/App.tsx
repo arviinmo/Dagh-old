3fccf837e0a0a5e9dd4f344d8607d431689f677a
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-04-26 16:39:26 +0430

    add styled-components for typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
-
-const StyledCounter = styled.div`
-  /* ... */
-`;
 
 function App() {
-  return (
-    <div className="">
-    </div>
-  );
+  return <div className=""></div>;
 }
 
 export default App;
