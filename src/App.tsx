863a87799c99e36b2cb50212c3b230f85ed356b2
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-04-27 20:50:37 +0430

    add style to app.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 62rem;
+  height: 450px;
+  background-color: red;
+`;
 
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="app">
+      <Container>
+        <h1>hello</h1>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
