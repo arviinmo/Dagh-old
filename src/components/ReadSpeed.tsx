09459a2614618c09201fa7d3a54e44411e1c4c6c
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-05-05 23:15:40 +0430

    add Readspeed component

--- src/components/ReadSpeed.tsx
+++ src/components/ReadSpeed.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
 function ReadSpeed() {
   return (
     <Container>ReadSpeed</Container>
