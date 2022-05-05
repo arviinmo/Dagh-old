9ec524e43fef5ea81d98b4d3b6f911998a5ebdc5
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-05-05 23:09:15 +0430

    add some basic style

--- src/components/WordCalculator.tsx
+++ src/components/WordCalculator.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 
-
 const Container = styled.div`
-  background-color: blue
-`
+  background-color: blue;
+  position: relative;
+  border-right: 2px solid yellow;
+`;
 
 function WordCalculator() {
-  return (
-    <Container>WordCalculator</Container>
-  )
+  return <Container>WordCalculator</Container>;
 }
 
-export default WordCalculator
+export default WordCalculator;
