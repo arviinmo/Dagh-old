f3c69467927af456453e2812fc451acfc7c29b0b
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-05-11 12:04:42 +0430

    add coffee component style

--- src/components/Coffee.tsx
+++ src/components/Coffee.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Button = styled.a`
-  line-height: 2;
-  height: 5rem;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  height: auto;
+  width: fit-content;
   text-decoration: none;
-  display:inline-flex;
-  color: #FFFFFF;
-  background-color: #FF813F;
+  display: inline-flex;
+  color: #ffffff;
+  background-color: #ff813f;
   border-radius: 5px;
   border: 1px solid transparent;
   padding: 0.7rem 1rem 0.7rem 1rem;
-  font-size: 2rem;
-  letter-spacing: 0.6px;
+  font-size: 1rem;
+  letter-spacing: 0.1px;
   box-shadow: 0px 1px 2px rgba(190, 190, 190, 0.5);
   transition: 0.3s all linear;
   font-family: cursive;
-  &:hover, &:active, &:focus {
+  margin: 0 20px 20px 0;
+  &:hover,
+  &:active,
+  &:focus {
     text-decoration: none;
     box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5);
     opacity: 0.85;
-    color:#FFFFFF;
+    color: #ffffff;
   }
 `;
 
@@ -34,15 +40,18 @@ const Image = styled.img`
 `;
 
 const Text = styled.span`
-  margin-left: 15px;
-  font-size: 2rem;
+  margin-left: 5px;
+  font-size: 1.25rem;
   vertical-align: middle;
 `;
 
 function Coffee() {
   return (
-    <Button target="_blank" href="https://www.buymeacoffee.com/agney">
-      <Image src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee" />
+    <Button target="_blank" href="https://www.buymeacoffee.com/smigle">
+      <Image
+        src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+        alt="Buy me a coffee"
+      />
       <Text>Buy me a coffee</Text>
     </Button>
   );
--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import Coffee from '../components/Coffee';
+import React from "react";
+import Coffee from "../components/Coffee";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #ffc271;
+  width: 100%;
+`;
+
+const Button = styled.div`
+  
+`;
 
 function Footer() {
   return (
-    <div>
-      <Coffee />
-    </div>
-  )
+    <Container>
+      <Button>
+        <Coffee />
+      </Button>
+    </Container>
+  );
 }
 
-export default Footer
+export default Footer;
